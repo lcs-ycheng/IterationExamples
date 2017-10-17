2dfe1db41056ac18d3be4ac71e52cfcc2685b90d
ycheng <ycheng@lcs-mba3103.lcs.on.ca>
2017-10-16 21:00:09 -0400

    Brighter circle

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -20,10 +20,10 @@ canvas.drawShapesWithFill = true
 canvas.drawShapesWithBorders = false
 
 //changing the horizontal position
-for x in stride(from: 5, to: 325, by: 50) {
+for x in stride(from: 25, to: 325, by: 50) {
  
     //changing the vertical position
-    for y in stride(from: 5, to: 325, by: 50){
+    for y in stride(from: 25, to: 325, by: 50){
 
         //chose the color of the circle
             let hue = random(from: 0, toButNotIncluding: 360)
@@ -31,11 +31,11 @@ for x in stride(from: 5, to: 325, by: 50) {
         for i in stride(from: 50, to: 25, by: -10){
 
         //Set color
-        canvas.fillColor = Color(hue: i, saturation: hue, brightness: 100, alpha: 100)
+        canvas.fillColor = Color(hue: hue, saturation: i+20, brightness: 100, alpha: 100)
         
         //draw the circle
+           canvas.drawEllipse(centreX: x, centreY: y, width: i, height: i)
            
-            canvas.drawRectangle(bottomLeftX: x, bottomLeftY: y, width: i, height: i)
           
     
 }
